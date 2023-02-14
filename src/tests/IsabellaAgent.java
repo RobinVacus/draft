2199335d7060684314b520c4ee72671a967f0ee3
RobinVacus <rvacus@irif.fr>
2023-02-14 22:21:54 +0100

    Updated Isabella's protocol

--- src/tests/IsabellaAgent.java
+++ src/tests/IsabellaAgent.java
@@ -14,8 +14,28 @@ public class IsabellaAgent extends Agent<Integer> {
 	@Override
 	public void update(ArrayList<Integer> samples) {
 		
+		// Number of 1 in the sample
 		int count = Utils.count(samples,1);
 		
+		// Unanimity of 1 : adopt opinion 1
+		if (count == sampleSize) {
+			opinion = 1;
+			return;
+		}
+		
+		// Unanimity of 0 : adopt opinion 0
+		if (count == 0) {
+			opinion = 0;
+			return;
+		}
+		
+		// Break ties randomly
+		if (count == sampleSize - count) {
+			opinion = Utils.random.nextInt(2);
+			return;
+		}
+		
+		// Compute the minority
 		int minority = 0;
 		int minorityCount = sampleSize - count;
 		if (count < sampleSize - count) {
@@ -25,6 +45,7 @@ public class IsabellaAgent extends Agent<Integer> {
 		
 		double proportion = ((double) minorityCount)/sampleSize;
 		
+		// Apply the protocol
 		if (proportion > 0.1 || Utils.random.nextDouble() > proportion) {
 			opinion = minority;
 		} else {
